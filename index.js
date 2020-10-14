7ce0844231fcae8d3693ad3277f2328108c3147a
Grantjos12 <grantjoslyn@gmail.com>
2020-10-13 20:33:58 -0500

    added variables and objects

---
+++ index.js
@@ -0,0 +1,43 @@
+const inquirer = require("inquier"); //inquire
+const fs = require("fs"); //filesystem 
+const                 //markdown file??
+
+///////starting the inquire function 
+inquirer.prompt([
+  {
+    type: "input",
+    message: "What is your project Title?",
+    name: "title",
+  },
+  {
+    type: "input",
+    message: "What is the project Description?",
+    name: "description",
+  },
+  {
+    type: "input",
+    message: "What are the installation instructions?",
+    name: "installation_instructions",
+  },
+  {
+    type: "input",
+    message: "What Usage Information should a user know for the Application?",
+    name: "usage_information",
+  },
+  {
+    type: "input",
+    message: "What are the Contribution Guidlines?",
+    name: "contribution_guidlines",
+  },
+  //Description
+  //Installation Instructions
+  //Usage Information
+  //Contribution Guidlines
+  //Test Instructions
+  //Liscense
+  //GitHub
+]);
+///promise to write to a markdown file 
+
+
+// function writerToFile()

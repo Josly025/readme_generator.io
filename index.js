fb93f4ddbd9103ab687d0b978dadfd201d7f0e09
Grantjos12 <grantjoslyn@gmail.com>
2020-10-14 17:36:31 -0500

    finished promise

--- index.js
+++ index.js
@@ -1,43 +1,63 @@
-const inquirer = require("inquier"); //inquire
+const inquirer = require("inquirer"); //inquire
 const fs = require("fs"); //filesystem
 const path = require("path");
 const markdown = require("."); //markdown file??
 
 ///////starting the inquire function
-inquirer.prompt([
-  {
-    type: "input",
-    message: "What is your project Title?",
-    name: "title",
-  },
-  {
-    type: "input",
-    message: "What is the project Description?",
-    name: "description",
-  },
-  {
-    type: "input",
-    message: "What are the installation instructions?",
-    name: "installation_instructions",
-  },
-  {
-    type: "input",
-    message: "What Usage Information should a user know for the Application?",
-    name: "usage_information",
-  },
-  {
-    type: "input",
-    message: "What are the Contribution Guidlines?",
-    name: "contribution_guidlines",
-  },
-  //Description
-  //Installation Instructions
-  //Usage Information
-  //Contribution Guidlines
-  //Test Instructions
-  //Liscense
-  //GitHub
-]);
-///promise to write to a markdown file
+inquirer
+  .prompt([
+    {
+      type: "input",
+      message: "What is your GitHub username?",
+      name: "github",
+    },
+    {
+      type: "input",
+      message: "What is your project Title?",
+      name: "title",
+    },
+    {
+      type: "input",
+      message: "What is the project Description?",
+      name: "description",
+    },
+    {
+      type: "input",
+      message: "What are the Installation Instructions?",
+      name: "installation_instructions",
+    },
+    {
+      type: "input",
+      message: "What Usage Information should a user know for the Application?",
+      name: "usage_information",
+    },
+    {
+      type: "input",
+      message: "What are the Contribution Guidlines?",
+      name: "contribution_guidlines",
+    },
+    {
+      type: "input",
+      message: "What are the Contribution Guidlines?",
+      name: "contribution_guidlines",
+    },
+  ])
+  .then(function (answers) {
+    console.log(answers);
 
-// function writerToFile()
+    let markdown = `${answers.github}
+     ${answers.title}
+     ${answers.description}
+     ${answers.installation_instructions} 
+     ${answers.usage_information}
+     ${answers.contribution_guidlines}`;
+
+    fs.writeFile("README.md", markdown, function (error) {
+      if (error) {
+        console.log("There was an error!");
+        return error;
+      }
+    });
+  });
+
+//inquirer, path, markdown, fs
